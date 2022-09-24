1ed9061987a5b81dc7079e0a9eae5021c2cc25f6
Francis Muriithi <mufraggie@gmail.com>
2022-09-25 00:13:00 +0300

    Last commit 24th

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -3,7 +3,6 @@ import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
-
 function AccountContainer() {
   return (
     <div>
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,15 +1,22 @@
-import React from "react";
+import {useEffect} from "react";
 
-function Transaction(props) {
-  const {date, description, category,  amount} = props;
-  return (
-    <tr>
-      <td>{date}</td>
-      <td>{description}</td>
-      <td>{category}</td>
-      <td>{amount}</td>
-    </tr>
-  );
+const url= "http://localhost:8001/transactions";
+function Transaction() {
+  useEffect(()=>{
+    fetch(url).then((data) => {return data.json()
+    }).then((completedata)=>{
+      console.log(completedata);
+    completedata.map((values)=>{
+      return (
+        `<tr>
+        <td>${values.date}</td>
+      <td>${values.description}</td>
+      <td>${values.category}</td>
+      <td>${values.amount}</td>
+      </tr>`
+      )
+    })
+    })
+  }, [])
 }
-
 export default Transaction;
